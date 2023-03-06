250337fd6b759a622445faa9d28d3ede1c06d2e6
birhanu-moges <birhanu18@gmail.com>
2023-03-06 11:13:01 -0800

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - finds the loop in a linked list
+ * @list: head pointer to list
+ *
+ * Return: 1 on success or 0 on failure
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *sp, *fp, *tmp;
+	unsigned int flag = 0;
+
+	if (head == NULL)
+		return (NULL);
+	sp = fp = tmp = head;
+	while (sp && fp && fp->next)
+	{
+		sp = sp->next;
+		fp = sp->next->next;
+		if (sp == fp)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	return (flag);
+}
