83d12337cff33ef501c60997fcec2924957f9d3a
birahnu-moges <birhanu18@gmail.com>
2023-03-09 05:55:33 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
     if n <= 0:
-        print("{:d} arguments".format(n))
+        print("{:d} arguments:".format(n))
     elif n > 0:
-        print("{:d} arguments".format(n))
+        print("{:d} arguments:".format(n))
         arglist = sys.argv[1:]
         index = 1
         for element in arglist:
