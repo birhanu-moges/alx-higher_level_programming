5e0cd0d8ae811226eb6b766c0c1b8dc26b85a2b3
birahnu-moges <birhanu18@gmail.com>
2023-03-11 23:25:19 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,13 @@
 
 def element_at(my_list, idx):
     """Retrieves an element from a list at index idx
+    
     Args:
+        my_list: list argument 
+        idx: given index in the list
 
-    my_list: list argument 
-    idx: given index in the list
+        Returns:
+            element at index idx
     """
     if idx < 0 or idx >= len(my_list):
         return None
