979118aca9cadd8f0454e406ffc3873705fee0ac
birahnu-moges <birhanu18@gmail.com>
2023-03-12 10:26:06 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,15 +9,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Returns:
         tuple with 2 values
     """
-    new_tuple = ()
     if len(tuple_a) == 1:
-        tuple_a = tuple_a + (0)
+        tuple_a = (tuple_a[0], 0)
     if len(tuple_a) == 0:
         tuple_a = (0, 0)
     if len(tuple_b) == 1:
-        tuple_b = tuple_b + (0)
+        tuple_b = (tuple_b[0], 0) 
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
-    for i in range(2):
-        new_tuple = (tuple_a[i] + tuple_b[i])
-    return new_tuple
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
