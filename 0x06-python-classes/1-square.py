cb31bab3d93fb90cd2ad18afd0b215a11874ed34
birahnu-moges <birhanu18@gmail.com>
2023-03-28 11:44:38 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module for Python project 0x06
+"""
+
+class Square:
+    """class defined for square
+    """
+    def __init__(self, size):
+        self.__size = size
