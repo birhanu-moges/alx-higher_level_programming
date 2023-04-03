247781a19b7b0e0c65503b2c24401efc4026e69d
birahnu-moges <birhanu18@gmail.com>
2023-04-03 11:31:37 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,10 +8,14 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        if type(width) is not int or type(height) is not int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if widht < 0 or height < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.width = width
         self.height = height
 
