6652a8745fdbd387390708959a63ce0ecc580397
birahnu-moges <birhanu18@gmail.com>
2023-04-03 11:36:22 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,6 +8,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height
+        """
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
