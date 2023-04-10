aa30f21c8db2fb235054595bf8bfdb36bb5a4cca
birahnu-moges <birhanu18@gmail.com>
2023-04-10 12:59:11 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """check object is an instance of a specified class"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """Returns True if object is an instance of a class"""
 
     if not type(obj) is a_class and issubclass(type(obj), a_class):
