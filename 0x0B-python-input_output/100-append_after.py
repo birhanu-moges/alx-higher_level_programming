480b88ff54cc8d18b733fce4a95fdcc0d3d2e5d5
birahnu-moges <birhanu18@gmail.com>
2023-04-11 12:18:58 -0700

    task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""write_file module.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    search a string and replace with new string
+    """
+    out = ""
+    with open(filename, 'r') as fd:
+        for line in fd:
+            out += line
+            if search_string in line:
+                out += new_string
+
+    with open(filename, 'w') as fd:
+        f.write(out)
