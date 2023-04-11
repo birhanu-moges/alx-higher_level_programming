c67e909cef737f322627935dfcab94215407cd3f
birahnu-moges <birhanu18@gmail.com>
2023-04-11 10:32:57 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to write string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Module to write string utf text to a file
+    Args:
+        filename: file name
+        text: text content to be written to a file
+    """
+    with open(filename, 'w', encoding='utf-8') as fd:
+        print(fd.write(text))
