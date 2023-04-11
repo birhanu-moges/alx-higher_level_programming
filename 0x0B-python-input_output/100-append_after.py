94512b98115f4105d9af392a5219469112651500
birahnu-moges <birhanu18@gmail.com>
2023-04-11 12:48:41 -0700

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,8 +10,8 @@ def append_after(filename="", search_string="", new_string=""):
     out = ""
     with open(filename, 'r') as fd:
         for line in fd:
-            for search_string in line:
-                if search_string == new_string:
+            for word in line:
+                if woed == search_string and search_string == new_string:
                     out += new_string
                 else:
                     Out += search_string
