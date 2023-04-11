f9a37207289222565ef69b6a48e5a46214625a99
birahnu-moges <birhanu18@gmail.com>
2023-04-11 10:49:59 -0700

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,8 +2,12 @@
 """
 Module to return JSON representation of an object
 """
-
-
 import json
+
+
 def to_json_string(my_obj):
+    """function to rerurn JSON representaion of an object
+    Args:
+        my_obj: object
+    """
     return json.dumps(my_obj)
