d3f10529061686fccfd731b8825e723a5314dd20
birahnu-moges <birhanu18@gmail.com>
2023-04-11 12:55:39 -0700

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,9 @@ def append_after(filename="", search_string="", new_string=""):
     out = ""
     with open(filename, 'r') as fd:
         for line in fd:
-            for word in line:
-                if woed == search_string and search_string == new_string:
-                    out += new_string
-                else:
-                    Out += search_string
+            out += line
+            if search_string in line:
+                out += new_string
 
     with open(filename, 'w') as fd:
-        f.write(out)
+        fd.write(out)
