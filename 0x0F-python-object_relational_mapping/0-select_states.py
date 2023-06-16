c043e6e17dc27d4c631cd419325fac40a2cd46f9
birahnu-moges <birhanu18@gmail.com>
2023-06-16 10:33:22 -0700

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
 import sys
 import MySQLdb
 
@@ -12,3 +9,5 @@ if __name__ == "__main__":
     con.execute("SELECT * FROM `states`")
     for state in con.fetchall():
          print(state)
+    con.close()
+    db.close()
