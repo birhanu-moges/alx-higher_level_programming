910c8443d154eae04a06922d1b8b7a024ed3faf5
birahnu-moges <birhanu18@gmail.com>
2023-06-18 11:38:32 -0700

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,11 +8,11 @@ from model_city import City
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
+    Base.metadata.create_all(engine)
     for instance in session.query(State).order_by(State.id):
         print("{}: {}".format(instance.id, instance.name))
     session.close()
